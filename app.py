5d0e4b039c3f7e570588ee4a6ee8eb27837c621e
yadavanushka266 <prakashanushka266@gmail.com>
2025-11-05 21:55:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+tfidf=pickle.load(open("C:\Users\hp\OneDrive\Desktop\sms spam classifier\vectoriser.pkl",'rb'))
+model=pickle.load(open("C:\Users\hp\OneDrive\Desktop\sms spam classifier\model.pkl",'rb'))
+
+def transform_text(text):
+  text = text.lower()
+  text = nltk.word_tokenize(text)
+  y = []
+  for i in text:
+    if i.isalnum():
+      y.append(i)
+
+  text = y[:]
+  y.clear()
+
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:
+      y.append(i)
+    text=y[:]
+    y.clear()
+
+    for i in text:
+      y.append(ps.stem(i))
+
+  return " ".join(y)
+
+st.title('SMS Spam Classifier')
+input_sms=st.text_area('Enter your message')
+if st.button('Predict'):
+  #Preprocessing
+  transform_sms=transform_text(input_sms)
+
+  #Vectorize
+  vector_input=tfidf.transform([transform_sms])
+
+  #predict
+  result=model.predict(vector_input)[0]
+  
+  #Display
+  if result == 1:
+    st.header('Spam')
+  else:
+    st.header('Not Spam')
